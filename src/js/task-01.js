a16a9e6e722a570a95e69505ba6c1313601f75ec
MishynaIra <gortenzia1987@gmail.com>
2022-10-03 12:19:36 +0300

    add 9 измение цвета по нажатию на кнопку

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,23 +1,23 @@
 //Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-const categories = document.querySelectorAll('.item');
-console.log('');
+const categories = document.querySelectorAll(".item");
+console.log("");
 console.log(`Number of categories: ${categories.length}`);
-console.log('');
+console.log("");
 
 //Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-categories.forEach(element => {
-   const group = [...element.children  ];
+categories.forEach((element) => {
+  const group = [...element.children];
 
-   group.forEach(element => {
-       if (element.tagName === 'H2') {
-           console.log(`Category: ${element.textContent}`);
-       }
-   });
+  group.forEach((element) => {
+    if (element.tagName === "H2") {
+      console.log(`Category: ${element.textContent}`);
+    }
+  });
 
-   group.forEach(element => {
-       if (element.tagName === 'UL') {
-           console.log(`Elements: ${element.children.length}`);
-       }
-   });
-   console.log(' ');
+  group.forEach((element) => {
+    if (element.tagName === "UL") {
+      console.log(`Elements: ${element.children.length}`);
+    }
+  });
+  console.log(" ");
 });
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const color = document.querySelector('.color');
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor(); //изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color
+  color.textContent = getRandomHexColor();
+});
+
+
+
+
