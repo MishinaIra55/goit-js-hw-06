1100ae856ee205d30fa9c1a7e6865e43e12320c1
MishynaIra <gortenzia1987@gmail.com>
2022-10-04 14:06:27 +0300

    add task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,16 +1,34 @@
-// // function getRandomHexColor() {
-// //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// // }
-
-
 // Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
-const collectionElement = document.getElementById('controls');
 
- const div = document.createElement('div');
-// collectionElement.append(div);
-// console.log(div);
+
+ const createButton = document.querySelector('[data-create]');
+ createButton.addEventListener('click', () => {
+   let value = document.querySelector('input').value;
+   createBoxes(Number(value));
+ });
+
+ const removeButton = document.querySelector('[data-destroy]');
+ removeButton.addEventListener('click', () => {
+   document.querySelector('#boxes').innerHTML = '';
+   document.querySelector('input').value = '';
+ })
 
 
 function createBoxes(amount) {
-   
+   let param = 30;
+   for(let i = 1; i <= amount; i += 1){
+      let div = document.createElement('div');
+      let boxes = document.querySelector('#boxes');
+      div.style.width = param + 'px';
+      div.style.height = param + 'px';
+      div.style.backgroundColor = getRandomHexColor();
+      param += 10;
+      boxes.append(div);
+   }
 }
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215)
+     .toString(16)
+     .padStart(6, 0)}`;
+ }
